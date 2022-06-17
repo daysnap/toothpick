bcf88ab9be38155c76b43f2496c90cb2f126222d
chenzhigang <979703986@qq.com>
2022-06-17 14:10:55 +0800

    subcode

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,103 @@
 
 import { log, nf } from './utils'
 
+interface AsyncMethodOptionLike {
+  success?: (...args: any[]) => void
+}
+
+type PromisifySuccessResult<
+  P,
+  T extends AsyncMethodOptionLike
+> =
+  P extends { success: any }
+  ? void
+  : P extends { fail: any }
+  ? void
+  : Promise<Parameters<Exclude<T['success'], undefined>>[0]>
+
+interface FailCallbackResult {
+  code?: number
+  message?: string
+}
+
+enum Code {
+  SUCCESS = 0,
+  FAIL = 999
+}
+
+interface SuccessCallbackResult<T> {
+  code: number
+  message?: string
+  data?: T
+}
+
+interface SuccessCallback<T> {
+  (result: SuccessCallbackResult<T>): void
+}
+
+interface FailCallback {
+  (err: FailCallbackResult): void
+}
+
+declare var DsBox:{
+  call<T> (
+    success: SuccessCallback<T>,
+    fail: FailCallback,
+    service: string,
+    action: string,
+    params: any
+  ): void
+}
+
+interface CoreOptions<T> {
+  success?(data: T): void
+  fail?: FailCallback
+  service?: string
+  action?: string
+  params?: string
+}
+
+
+export const core = <T>(options: CoreOptions<T>) => {
+  const {
+    success,
+    fail,
+    service,
+    action,
+    params
+  } = options
+  DsBox.call<T>(
+    res => {
+      log(service, action, '请求返回 => ', res)
+      const { code, data } = res
+      code === Code.SUCCESS ? success(data) : fail(res)
+    },
+    err => {
+      log(service, action, '请求错误 => ', err)
+      fail(err)
+    },
+    service,
+    action,
+    params
+  )
+}
+
+
+// interface BootstrapOption extends CoreOptions{
+//
+// }
+
+
+
+
+export const bootstrap = (options: {
+  success?
+}) => {
+
+}
+
+core<string>({
+  success(data) {
+
+  }
+})
