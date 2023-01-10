083fd91caa35cee26839ecca969fd0868f836c70
chenzhigang <979703986@qq.com>
2023-01-10 19:22:44 +0800

    chore: package.json

--- packages/boss/src/App.tsx
+++ packages/boss/src/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App bg-red-100">
+    <div className="App bg-red-100"
+    >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
