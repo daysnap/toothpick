924835e1e3f7aa59c84314edeb13467a52844dcd
chenzhigang <979703986@qq.com>
2023-01-10 13:27:10 +0800

    chore: service

---
+++ packages/service/src/index.ts
@@ -0,0 +1,13 @@
+import http from 'http'
+import { Server } from 'socket.io'
+
+const server = http.createServer()
+
+const io = new Server(server)
+
+io.on('connection', client => {
+  client.on('event', data => { /* … */ })
+  client.on('disconnect', () => { /* … */ })
+})
+
+server.listen(3000)
