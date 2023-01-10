cb2b7bb6d67fb6c32dc4ced7386a65b93438d019
chenzhigang <979703986@qq.com>
2023-01-10 14:40:55 +0800

    chore: nodemon

--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -4,11 +4,16 @@ import { Server } from 'socket.io'
 const server = http.createServer()
 const io = new Server(server)
 
-io.on('connection', client => {
-  client.on('event', data => { /* … */ })
-  client.on('disconnect', () => { /* … */ })
+io.on('connection', (client) => {
+  client.on('event', (data) => {
+    /* … */
+    console.log('data => ', data)
+  })
+  client.on('disconnect', () => {
+    /* … */
+  })
 })
 
 server.listen(3000)
 
-console.log(`服务已启动`)
+console.log(`socket 服务已启动`)
