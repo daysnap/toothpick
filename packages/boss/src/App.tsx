d5653d1fdbcfc1561f4f65409c1ca97d548f59a1
chenzhigang <979703986@qq.com>
2023-01-15 22:32:56 +0800

    fix: boss wx 样式兼容

--- packages/boss/src/App.tsx
+++ packages/boss/src/App.tsx
@@ -1,4 +1,5 @@
 import { ConfigProvider, Watermark } from 'antd'
+import { StyleProvider } from '@ant-design/cssinjs'
 import zhCN from 'antd/es/locale/zh_CN'
 import classnames from 'classnames'
 import Router from '@/router'
@@ -7,33 +8,36 @@ import classes from './index.module.scss'
 
 export default function App() {
   return (
-    <ConfigProvider
-      locale={zhCN}
-      theme={{
-        token: {
-          colorPrimary: '#00b96b',
-        },
-      }}
-    >
-      <SocketClient>
-        <Watermark content="Toothpick">
-          <div
-            className={classnames(
-              'fixed -z-50 top-[-50px] right-[-600px] h-[1000px] w-[1000px] rounded-full',
-              classes.decorator1,
-            )}
-          />
-          <div
-            className={classnames(
-              'fixed -z-50 top-[-200px] left-[-600px] h-[1000px] w-[1000px] rounded-full',
-              classes.decorator2,
-            )}
-          />
-          <div className="relative box-border mx-auto min-h-screen max-w-[768px] bg-gray-300/10">
-            <Router />
-          </div>
-        </Watermark>
-      </SocketClient>
-    </ConfigProvider>
+    // https://ant.design/docs/react/compatible-style-cn
+    <StyleProvider hashPriority="high">
+      <ConfigProvider
+        locale={zhCN}
+        theme={{
+          token: {
+            colorPrimary: '#00b96b',
+          },
+        }}
+      >
+        <SocketClient>
+          <Watermark content="Toothpick">
+            <div
+              className={classnames(
+                'fixed -z-50 top-[-50px] right-[-600px] h-[1000px] w-[1000px] rounded-full',
+                classes.decorator1,
+              )}
+            />
+            <div
+              className={classnames(
+                'fixed -z-50 top-[-200px] left-[-600px] h-[1000px] w-[1000px] rounded-full',
+                classes.decorator2,
+              )}
+            />
+            <div className="relative box-border mx-auto min-h-screen max-w-[768px] bg-gray-300/10">
+              <Router />
+            </div>
+          </Watermark>
+        </SocketClient>
+      </ConfigProvider>
+    </StyleProvider>
   )
 }
